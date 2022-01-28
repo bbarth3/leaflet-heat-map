6d39b1d7de641c350c12cb0001811361b1c32dae
Brendan Barth <98061278+bbarth3@users.noreply.github.com>
2022-01-27 23:47:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var map = L.map('map').setView([37.7, -122.4], 10);
 
-var Stamen_Terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+var Stamen_Terrain = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
 	subdomains: 'abcd',
 	minZoom: 0,
 	maxZoom: 18,
