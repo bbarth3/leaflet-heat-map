82332a6e65f5c876b84ca3e323bd407047d21bb1
Brendan Barth <98061278+bbarth3@users.noreply.github.com>
2022-01-27 23:08:32 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+var map = L.map('map').setView([37.7, -122.4], 10);
+
+var Stamen_Terrain = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+}).addTo(map);
+
+$.getJSON('https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson', function(data) {
+  var coordinatesOnly = data.features.map(function(feature) {
+    return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
+  });
+
+  var heat = L.heatLayer(coordinatesOnly).addTo(map);
+});
+
